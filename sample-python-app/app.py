2ba4334b5b03ba236ed1428e9583f99c3e55b4ce
keerthana <keerthanakeerthu31.kk@gmail.com>
2024-02-19 22:03:45 +0530

    changes

--- sample-python-app/app.py
+++ sample-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hi there, its me Keerthana!'
+    return 'Hi there, its me Keerthana!!'
 
 if __name__ == '__main__':
     app.run()
